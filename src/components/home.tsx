7cb5df9d53805ddc7ccd22bb5cbfcb7db285c3b2
omarhsouna <omar.hsouna@gmail.com>
2022-06-26 10:36:33 +0200

    add prettier config

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -15,20 +15,25 @@ function Home({ setActiveTab }: Props) {
     }
   }, [onScreen]);
   return (
-    <div tw="bg-cover-img bg-no-repeat bg-cover bg-cover-pos py-24 md:py-40 flex" id="home">
+    <div
+      tw="bg-cover-img bg-no-repeat bg-cover bg-cover-pos py-24 md:py-40 flex"
+      id="home"
+    >
       <div tw="pt-14 mx-auto ml-8 sm:ml-32" ref={ref}>
-        <span tw="mb-3 text-xs md:text-base text-third
+        <span
+          tw="mb-3 text-xs md:text-base text-third
              uppercase tracking-home-spacing"
         >
           Welcome to my world
         </span>
         <h1 tw="text-white text-4xl md:text-[54px] leading-10 md:leading-[68px] font-black mb-5">
           Hi, I&apos;m Omar Hsouna
-          <br />
-          {" "}
-          <div tw="text-secondary">JS Developer.</div>
+          <br /> <div tw="text-secondary">JS Developer.</div>
         </h1>
-        <h2 tw="text-3xl md:text-[54px] text-white font-bold"> based in Tunisia.</h2>
+        <h2 tw="text-3xl md:text-[54px] text-white font-bold">
+          {" "}
+          based in Tunisia.
+        </h2>
       </div>
     </div>
   );
